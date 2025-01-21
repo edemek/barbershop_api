c793404f60be1a8b503c7c689f007d8e986f2935
edemek <edemek28@gmail.com>
2025-01-21 15:06:11 +0000

    work on shop

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Barbershop\BarbershopController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum', 'admin')->get('/users', [AuthController::class, 'index']);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('barbershops', BarbershopController::class);
+});
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,7 @@
 <?php
 
-use App\Http\Controllers\AuthController; // Ensure this class exists in the specified namespace
-use App\Http\Controllers\SmsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Route pour envoyer l'OTP
-Route::post('/send-otp', [SmsController::class, 'sendOtp']);
-
-// Route pour vérifier l'OTP
-Route::post('/verify-otp', [AuthController::class, 'verifyOtp']);
