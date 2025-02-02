f809bcf15a7203b5f07e50a051c4caf49ee1be89
Harry.kouevi <harry.kouevi@gmaol.com>
2025-02-02 12:39:28 +0000

    change the firstpage of the Api

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'api_name' => 'Barber Management API',
+        'version' => '1.0',
+        'description' => 'This API allows you to manage barbers, including creating, updating, and deleting barber records.',
+        'endpoints' => [ ]
+    ]);
 });
